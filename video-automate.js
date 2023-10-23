b34d2f9f76b151eb2afa4308764adf4722a95c84
srthomasleonard <140288461+srthomasleonard@users.noreply.github.com>
2023-10-23 10:02:24 -0700

    Create video-automate.js

---
+++ video-automate.js
@@ -0,0 +1,43 @@
+var video = document.getElementById("video"),
+  fraction = 0.8;
+
+function checkScroll() {
+  var x = video.offsetLeft,
+    y = video.offsetTop,
+    w = video.offsetWidth,
+    h = video.offsetHeight,
+    r = x + w, //right
+    b = y + h, //bottom
+    visibleX,
+    visibleY,
+    visible;
+
+  visibleX = Math.max(
+    0,
+    Math.min(
+      w,
+      window.pageXOffset + window.innerWidth - x,
+      r - window.pageXOffset
+    )
+  );
+  visibleY = Math.max(
+    0,
+    Math.min(
+      h,
+      window.pageYOffset + window.innerHeight - y,
+      b - window.pageYOffset
+    )
+  );
+
+  visible = (visibleX * visibleY) / (w * h);
+
+  if (visible > fraction) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+checkScroll();
+window.addEventListener("scroll", checkScroll, false);
+window.addEventListener("resize", checkScroll, false);
